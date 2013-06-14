5ec51b9897d668373062e572fbbf6be0c5a56d0a
oli <oli@ignition-5d4c0e>
2013-06-14 00:41:08 -0300

    Punto Uno Andando

--- src/Do.groovy
+++ src/Do.groovy
@@ -2,13 +2,7 @@ class Do{
 	def static magic(){
 		BigDecimal.metaClass{
 			
-			ejecutar = { unBloque -> delegate.with(unBloque)  }
-			
-			doble = {->	new DobleOperation().execute(delegate)}
-			
-			redondear ={-> new RedondearOperation().execute(delegate)}
-			
-			sumar = {unNumero ->new SumarOperation(unNumero).execute(delegate)}
+			ejecutar = { unBloque -> new Chain(object:delegate).with unBloque  }
 			
 		}
 	}
--- test/ChainsOfOperationsTestCase.groovy
+++ test/ChainsOfOperationsTestCase.groovy
@@ -7,24 +7,6 @@ import org.junit.Test
 class ChainsOfOperationsTestCase {
 
 	@Test
-	void test_calcula_el_doble_de_dos_punto_uno(){
-		Do.magic()
-		assertEquals(4.2, 2.1.doble())
-	}
-	
-	@Test
-	void test_redondear_cuatro_punto_dos(){
-		Do.magic()
-		assertEquals(4.0,4.2.redondear(),0.1)
-	}
-	
-	@Test	
-	void test_suma_uno_y_dos(){
-		Do.magic() 
-		assertEquals(3.0, 1.0.sumar(2.0))
-	}
-	
-	@Test
 	void test_chaining_de_operaciones() {
 		Do.magic()
 		def resultado = 5.2.ejecutar {
